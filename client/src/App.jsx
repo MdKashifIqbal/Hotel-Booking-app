bac703af4f9f4eb5f056865a89d9e7d98af9f9d7
MdKashifIqbal <mdkashifiqbal2003@gmail.com>
2025-07-25 16:01:45 +0530

    room's details UI updated & my-booking page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './Pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './components/AllRooms';
 import RoomDetails from './Pages/RoomDetails';
+import MyBooking from './Pages/MyBooking';
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/rooms' element={<AllRooms/>}/>
         <Route path='/rooms/:id' element={<RoomDetails/>}/>
+        <Route path='/my-booking' element={<MyBooking/>}/>
       </Routes>
       <Footer/>
     </>
