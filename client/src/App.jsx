55dd0a5a78ce7de668263fae02a4abddd40eea0f
MdKashifIqbal <mdkashifiqbal2003@gmail.com>
2025-08-12 19:12:27 +0530

    hotel-Owner hotel-reg added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,29 @@ import Footer from './components/Footer';
 import AllRooms from './components/AllRooms';
 import RoomDetails from './Pages/RoomDetails';
 import MyBooking from './Pages/MyBooking';
+import HotelReg from './components/HotelReg';
+import Layout from './Pages/hotelOwner/Layout';
+import Dashboard from './Pages/hotelOwner/Dashboard';
+import AddRoom from './Pages/hotelOwner/AddRoom';
+import ListRoom from './Pages/hotelOwner/ListRoom';
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <>
-      {!isOwnerPath && <Navbar/>}
+     {!isOwnerPath && <Navbar/>}
+     {false && <HotelReg/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/rooms' element={<AllRooms/>}/>
         <Route path='/rooms/:id' element={<RoomDetails/>}/>
         <Route path='/my-booking' element={<MyBooking/>}/>
+        <Route path='/owner' element={<Layout/>}>
+            <Route index element={<Dashboard/>}/>
+            <Route path='add-room' element={<AddRoom/>}/>
+            <Route path='list-room' element={<ListRoom/>}/>
+        </Route>
       </Routes>
       <Footer/>
     </>
